c7385b68eb83ee8faee53b836740cfa4cb03d6e1
nitianabhigyan <abhigyankumar420@gmail.com>
2019-06-13 11:55:31 +0530

    first working column (name column)

--- FirstExp/Sample.js
+++ FirstExp/Sample.js
@@ -1,16 +1,15 @@
 var lang = "en-IN";
+var GlobalObj = null;
 var rate = 1;
 var list = null; //list.length to get the length if list at any time
-
+var voice = speechSynthesis.getVoices();
 var recognition = new webkitSpeechRecognition();
+var u = new SpeechSynthesisUtterance();
+u.lang = lang;
+u.rate = rate;
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.lang = lang;
-// var u = new SpeechSynthesisUtterance();
-// u.lang = lang;
-// u.rate = rate;
-
-
 
 function beep(){
 	a=new AudioContext() // browsers limit the number of concurrent audio contexts, so you better re-use'em
@@ -22,30 +21,38 @@ function beep(){
 	au.connect(a.destination)
 	au.gain.value=1
 	v.start(a.currentTime)
-	v.stop(a.currentTime+.250)
+	v.stop(a.currentTime+.200)
 }
+recognition.onresult = function(event) 	{ 
+	console.log('done'); // add for writing to content.
+	for (var i = event.resultIndex; i < event.results.length; ++i) {
+		if (event.results[i].isFinal){
+			final_tanscript = event.results[i][0].transcript;
+			console.log(final_tanscript);
+			GlobalObj.value = final_tanscript;
+			GlobalObj = null;
+			recognition.stop();
+			activate(num+1);
+		}
+	}
+}
+
 function listen(obj){
 	navigator.vibrate(200); // vibrate the device if Mobile
-	console.log("entered",obj);
 	beep();
+	GlobalObj = obj;
+	recognition.start();
 }
 
 function speak(obj){
-	var u = new SpeechSynthesisUtterance();
-	u.lang = lang;
-	u.rate = rate;
-
 	u.text = obj.getAttribute('tospeak');
 	console.log(u);
 	speechSynthesis.speak(u);
-	u.onend = listen(obj);
-	//speechSynthesis.onend = listen(obj);
+	u.addEventListener('end',function(event) { 
+		//console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+		listen(obj);
+	});
 }
-
-recognition.onresult = function(event) { 
-	console.log('done');
-}
-
 function activate(num){
 	if(num == 0){
 		list = document.querySelectorAll('input');
